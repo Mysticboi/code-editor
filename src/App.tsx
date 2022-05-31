0e5c4752612f885aa54282c80edc6a3db35f6d73
Walid Oulderra <38976111+Mysticboi@users.noreply.github.com>
2022-05-31 13:32:45 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Editor from '@monaco-editor/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Welcome to my Code Editor
+      <Editor
+        height="90vh"
+        defaultLanguage="javascript"
+        defaultValue="// comment"
+        theme="vs-dark"
+      />
     </div>
   );
 }
