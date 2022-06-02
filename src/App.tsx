82938a8aae394a8b310f084b92c71e9f978ec3d2
Walid Oulderra <38976111+Mysticboi@users.noreply.github.com>
2022-06-02 20:26:48 +0200

    remove tailwind add styled comp & eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Editor from '@monaco-editor/react';
+import styled from 'styled-components';
+
+const Paragraph = styled.p`
+  font-size: 20px;
+  text-align: center;
+  color: aquamarine;
+`;
 
 function App() {
   return (
     <div>
-      <p className="text-center text-xl">Welcome to my Code Editor</p>
+      <Paragraph>Welcome to my Code Editor</Paragraph>
       <Editor
         height="90vh"
         defaultLanguage="javascript"
