5e004784c8d36cceefe03042cf8d9efb68bf8c06
woulderra <walidoulderra@hotmail.fr>
2023-07-16 14:11:54 +0200

    update readme

--- src/tests/test-utils.tsx
+++ src/tests/test-utils.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, ReactElement } from 'react';
+import { ReactNode, ReactElement } from 'react';
 import { render as rtlRender } from '@testing-library/react';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
